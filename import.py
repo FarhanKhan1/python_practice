2011761c504d0ba4eb5c729ee559216e27b542f0
Farhan Khan <enggfarhan36@gmail.com>
2022-08-15 18:49:27 +0500

    Delete import.py

--- import.py
+++
@@ -1,8 +0,0 @@
-# import sys
-# sys.path.append('C:\\Users\\ICD\\Desktop\\python\\Test')
-# from Test import calculate
-# import pandas as pd
-# print(calculate.a)
-# calculate.sol()
-# obj = calculate.A()
-
